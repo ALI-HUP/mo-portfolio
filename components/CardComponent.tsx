37f94bd98aa451a4a578ea9e4fa8761bd6e2e614
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2024-12-18 14:32:46 +0330

    cards with new font

--- components/CardComponent.tsx
+++ components/CardComponent.tsx
@@ -11,7 +11,9 @@ const CardComponent: React.FC<CardComponentProps> = ({ title, link }) => {
     <Link href={link}>
       <div className="w-[310px] h-[490px] cursor-pointer relative flex flex-col bg-gradient-to-r from-indigo-950 to-sky-950 rounded-2xl justify-center items-center">
         <div className="text-center">
-          <span className="text-3xl font-semibold text-orange-200">{title}</span>
+          <span className="text-3xl font-semibold text-orange-200" style={{ fontFamily: "YatraOne" }}>
+            {title}
+          </span>
         </div>
       </div>
     </Link>
